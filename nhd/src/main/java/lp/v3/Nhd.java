58d3c69452844d8638792262cad019e9edf2d9ed
OE500 <yuanhy0055@163.com>
2016-05-05 18:07:30 +0800

    New application nhd(No Hear) v3

---
+++ nhd/src/main/java/lp/v3/Nhd.java
@@ -0,0 +1,13 @@
+package lp.v3;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class Nhd extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_nhd);
+    }
+}
