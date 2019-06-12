2173c1152dec52df2f6865f0b5b43d5462f71992
denk <denkyuan@qq.com>
2019-06-12 18:00:59 +0800

    AccessibilityService-1

--- app/src/main/java/com/ag/ndktest/MainActivity.java
+++ app/src/main/java/com/ag/ndktest/MainActivity.java
@@ -2,7 +2,9 @@ package com.ag.ndktest;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -19,6 +21,14 @@ public class MainActivity extends AppCompatActivity {
         // Example of a call to a native method
         TextView tv = findViewById(R.id.sample_text);
         tv.setText(stringFromJNI());
+
+        findViewById(R.id.btn_click).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(MainActivity.this, "我被点击了！", Toast.LENGTH_SHORT).show();
+                //Log.i(TAG, "onClick: 我被点击了！！！");
+            }
+        });
     }
 
     /**
