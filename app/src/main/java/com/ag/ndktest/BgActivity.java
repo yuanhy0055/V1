9d8e4b856086d6de8e5c7d2b40ce42fd3b3a7f73
denk <denkyuan@qq.com>
2019-06-13 18:32:44 +0800

    AccessibilityService-4: Activity for control

--- app/src/main/java/com/ag/ndktest/BgActivity.java
+++ app/src/main/java/com/ag/ndktest/BgActivity.java
@@ -1,13 +1,98 @@
 package com.ag.ndktest;
 
+import android.content.Context;
+import android.content.Intent;
+import android.provider.Settings;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.TextUtils;
+import android.util.Log;
+import android.view.View;
 
 public class BgActivity extends AppCompatActivity {
+
+    private static final String TAG = "BG_Activity";
+    private Intent intent;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_bg);
+
+        if (!isAccessibilitySettingsOn(BgActivity.this, ListeningService.class.getCanonicalName())) {
+            Intent intent = new Intent(Settings.ACTION_ACCESSIBILITY_SETTINGS);
+            startActivity(intent);
+        } else {
+            intent = new Intent(BgActivity.this, ListeningService.class);
+            startService(intent);
+        }
+
+
+        findViewById(R.id.start).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (!isAccessibilitySettingsOn(BgActivity.this, ListeningService.class.getCanonicalName())) {
+                    Intent intent = new Intent(Settings.ACTION_ACCESSIBILITY_SETTINGS);
+                    startActivity(intent);
+                } else {
+                    intent = new Intent(BgActivity.this, ListeningService.class);
+                    startService(intent);
+                }
+            }
+        });
+
+        findViewById(R.id.stop).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (intent != null) {
+                    stopService(intent);
+                }
+            }
+        });
+
+    }
+
+
+    /**
+     * 检测辅助功能是否开启
+     *
+     * @param mContext
+     * @param serviceName
+     * @return boolean
+     */
+    private boolean isAccessibilitySettingsOn(Context mContext, String serviceName) {
+        int accessibilityEnabled = 0;
+        // 对应的服务
+        final String service = getPackageName() + "/" + serviceName;
+        //Log.i(TAG, "service:" + service);
+        try {
+            accessibilityEnabled = Settings.Secure.getInt(mContext.getApplicationContext().getContentResolver(),
+                    android.provider.Settings.Secure.ACCESSIBILITY_ENABLED);
+            Log.v(TAG, "accessibilityEnabled = " + accessibilityEnabled);
+        } catch (Settings.SettingNotFoundException e) {
+            Log.e(TAG, "Error finding setting, default accessibility to not found: " + e.getMessage());
+        }
+        TextUtils.SimpleStringSplitter mStringColonSplitter = new TextUtils.SimpleStringSplitter(':');
+
+        if (accessibilityEnabled == 1) {
+            Log.v(TAG, "***ACCESSIBILITY IS ENABLED*** -----------------");
+            String settingValue = Settings.Secure.getString(mContext.getApplicationContext().getContentResolver(),
+                    Settings.Secure.ENABLED_ACCESSIBILITY_SERVICES);
+            if (settingValue != null) {
+                mStringColonSplitter.setString(settingValue);
+                while (mStringColonSplitter.hasNext()) {
+                    String accessibilityService = mStringColonSplitter.next();
+
+                    Log.v(TAG, "-------------- > accessibilityService :: " + accessibilityService + " " + service);
+                    if (accessibilityService.equalsIgnoreCase(service)) {
+                        Log.v(TAG, "We've found the correct setting - accessibility is switched on!");
+                        return true;
+                    }
+                }
+            }
+        } else {
+            Log.v(TAG, "***ACCESSIBILITY IS DISABLED***");
+        }
+        return false;
     }
 }
