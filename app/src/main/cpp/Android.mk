54e0dade3cd90104684e1318ebf93541f3fd80a3
yuanhy0055 <yuanhy0055@163.com>
2019-07-02 23:17:44 +0800

    test boost scope_ptr

--- app/src/main/cpp/Android.mk
+++ app/src/main/cpp/Android.mk
@@ -6,11 +6,12 @@ BOOST_INCLUDE_PATH	:= E:/PWB/yyboostlib/x86_64/include/boost-1_68
 BOOST_LIB_PATH		:= E:/PWB/yyboostlib/x86_64/lib
 
 LOCAL_MODULE    := ndk-test-lib
-LOCAL_SRC_FILES := main.cpp
+LOCAL_SRC_FILES := main.cpp \
+                   android_buf.cpp
 
 LOCAL_C_INCLUDES	:= $(BOOST_INCLUDE_PATH)
 LOCAL_LDFLAGS += -fPIE
 LOCAL_LDLIBS +=  -llog -ldl
-#OCAL_LDLIBS += -L$(BOOST_LIB_PATH) -lboost_system
+LOCAL_LDLIBS += -L$(BOOST_LIB_PATH) -lboost_system-clang-mt-x64-1_68 -lboost_thread-clang-mt-x64-1_68
 
 include $(BUILD_SHARED_LIBRARY)
--- app/src/main/cpp/main.cpp
+++ app/src/main/cpp/main.cpp
@@ -1,10 +1,69 @@
 #include <jni.h>
 #include <string>
+#include <iostream>
+
+#include <boost/scoped_ptr.hpp>
+#include <android/log.h>
+
+using namespace std;
+
+#define LOGD(...) ((void)__android_log_print(ANDROID_LOG_DEBUG, "TTGG", __VA_ARGS__))
+
+int c_main(void);
+int start_logger(const char *app_name);
 
 extern "C" JNIEXPORT jstring JNICALL
 Java_com_ag_ndktest_MainActivity_stringFromJNI(
         JNIEnv *env,
         jobject /* this */) {
+
+    start_logger("TTGG");
+    c_main();
+
     std::string hello = "Hello from C++";
     return env->NewStringUTF(hello.c_str());
+}
+
+class CMyClass
+{
+public:
+    explicit CMyClass()
+    {
+        cout << "CMyClass Construction function !" << endl;
+    }
+
+    ~CMyClass()
+    {
+        cout << "CMyClass Destruct function" << endl;
+    }
+
+    void MyFunc(void){
+
+        cout << "Do Something...." << endl;
+    }
+};
+
+void Function1(void)
+{
+    //boost::scoped_ptr是一个简单的智能指针，它
+    //能保证离开作用域后对象被自动释放，能避免由于
+    //忘记释放而导致的内存泄漏。
+    //验证运行的正确性是：打印：Now, Quit Function ...
+    //之后，打印：CMyClass Destruct function
+    boost::scoped_ptr<CMyClass> spMyClass(new CMyClass());
+    spMyClass->MyFunc();
+    cout << "Now, Quit Function ..." << endl;
+}
+
+int c_main(void)
+{
+    cout << "boost demo test start ..." << endl;
+    Function1();
+    cout << "boost demo test end ..." << endl;
+
+    while (1) {
+        sleep(1);
+    }
+
+    return 0;
 }
