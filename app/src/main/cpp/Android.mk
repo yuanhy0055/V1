0551512f274613522598ff5374a4c046a920300f
yuanhy0055 <yuanhy0055@163.com>
2019-07-22 16:45:51 +0800

    Test 3rd lib on X86

--- app/src/main/cpp/Android.mk
+++ app/src/main/cpp/Android.mk
@@ -2,8 +2,8 @@ LOCAL_PATH := $(call my-dir)
 
 include $(CLEAR_VARS)
 
-BOOST_INCLUDE_PATH	:= E:/PWB/yyboostlib/x86_64/include/boost-1_68
-BOOST_LIB_PATH		:= E:/PWB/yyboostlib/x86_64/lib
+BOOST_INCLUDE_PATH	:= C:/a/yylib/boostlib/x86/include/boost-1_68
+BOOST_LIB_PATH		:= C:/a/yylib/boostlib/x86/lib
 
 LOCAL_MODULE    := ndk-test-lib
 LOCAL_SRC_FILES := main.cpp \
@@ -13,6 +13,6 @@ LOCAL_SRC_FILES := main.cpp \
 LOCAL_C_INCLUDES	:= $(BOOST_INCLUDE_PATH)
 LOCAL_LDFLAGS += -fPIE
 LOCAL_LDLIBS +=  -llog -ldl
-LOCAL_LDLIBS += -L$(BOOST_LIB_PATH) -lboost_system-clang-mt-x64-1_68 -lboost_thread-clang-mt-x64-1_68
+LOCAL_LDLIBS += -L$(BOOST_LIB_PATH) -lboost_system-clang-mt-x32-1_68 -lboost_thread-clang-mt-x32-1_68
 
 include $(BUILD_SHARED_LIBRARY)
--- app/src/main/cpp/Application.mk
+++ app/src/main/cpp/Application.mk
@@ -1,5 +1,5 @@
 #APP_STL := gnustl_static
 APP_STL := c++_static
 APP_CPPFLAGS := -frtti -fexceptions
-APP_ABI := x86_64
+APP_ABI := x86
 APP_PLATFORM := android-8
